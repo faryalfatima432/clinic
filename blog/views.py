9f878d36dfb97c872152e7b14395a4d4f21ffef3
Hamna Tariq <hamnatariq663@gmail.com>
2025-11-26 17:01:42 +0500

    Refactor blog views to include comments and improve response rendering

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render,get_list_or_404,get_object_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse
-from .models import Blog
+from .models import Blog, Comment
+
 
 def blogs(request):
-    blogs=get_list_or_404(Blog)
-    return HttpResponse(blogs)
+    blogs = get_list_or_404(Blog)
+    return render(request, "blogs.html", {'blogs':blogs})
 
-def blogDetails(request,id):
-    blog=get_object_or_404(Blog,id=id)
-    print(blog.title,blog.content)
-    print(blog)
-    return HttpResponse(blog)
+
+def blogDetails(request, id):
+    blog = get_object_or_404(Blog, id=id)
+    comments = Comment.objects.filter(blog=id)
+    return render(request, "blog.html", {'blog': blog, 'comments': comments})
